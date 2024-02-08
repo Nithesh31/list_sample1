a10b63958b4fcce069eb1509f5b9781ee429c1d7
nitheshkotrike <nitheshkotrike1@gmail.com>
2024-02-08 17:56:04 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,16 @@
+names = ['sai','nani','karthik']
+
+print(len(names))
+# print(names[0])
+
+# for i in range(0,len(names),1):
+#     print(names[i])
+
+i=0
+# 0 < 3 -> sai
+# 1 < 3 -> nani
+# 2 < 3 -> karthik
+# 3 <= 3-> faile
+while(i<len(names)):
+    print(names[i])
+    i=i+1
